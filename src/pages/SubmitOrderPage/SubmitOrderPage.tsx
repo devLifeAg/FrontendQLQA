41a33bfa801a5b4e9965222e7a7f45396adc0a95
Phong <phongchau346@gmail.com>
2025-04-10 17:40:58 +0700

    cc

--- src/pages/SubmitOrderPage/SubmitOrderPage.tsx
+++ src/pages/SubmitOrderPage/SubmitOrderPage.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+// import React from 'react'
+// import { OrderPage } from '../OrderPage/OrderPage';
 
 // type FoodItem = {
 //     mon_id: number;
@@ -18,44 +19,44 @@ import React from 'react'
 //     mon_giamon: number;
 //     quantity: number;
 //   };
-//   type SubmitPayload = {
-//     b_id: number;
-//     items: {
-//       food_id: number;
-//       quantity: number;
-//     }[];
-//   };
+// //   type SubmitPayload = {
+// //     b_id: number;
+// //     items: {
+// //       food_id: number;
+// //       quantity: number;
+// //     }[];
+// //   };
   
 
-export const SubmitOrderPage = () => {
-    const payload = {
-        b_id: b_id, // giả sử b_id là số, ví dụ: 5
-        items: orderList.map((item) => ({
-          food_id: item.mon_id,       // từ OrderItem.mon_id
-          quantity: item.quantity     // từ OrderItem.quantity
-        }))
-      };
+// export const SubmitOrderPage = () => {
+//     const payload = {
+//         b_id: b_id, // giả sử b_id là số, ví dụ: 5
+//         item: OrderPage.map((item) => ({
+//           food_id: item.mon_id,       // từ OrderItem.mon_id
+//           quantity: item.quantity     // từ OrderItem.quantity
+//         }))
+//       };
     
-      try {
-        const response = await fetch("http://localhost/api/submit-order.php", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(payload)
-        });
+//       try {
+//         const response = await fetch("", {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json"
+//           },
+//           body: JSON.stringify(payload)
+//         });
     
-        if (response.ok) {
-          const result = await response.json();
-          alert("Gửi món thành công!");
-          // Có thể reset orderList nếu cần
-        } else {
-          alert("Gửi món thất bại!");
-        }
-      } catch (error) {
-        console.error("Lỗi khi gửi món:", error);
-        alert("Có lỗi xảy ra khi gửi món!");
-      }
-}
+//         if (response.ok) {
+//           const result = await response.json();
+//           alert("Gửi món thành công!");
+//           // Có thể reset orderList nếu cần
+//         } else {
+//           alert("Gửi món thất bại!");
+//         }
+//       } catch (error) {
+//         console.error("Lỗi khi gửi món:", error);
+//         alert("Có lỗi xảy ra khi gửi món!");
+//       }
+// }
 
   
