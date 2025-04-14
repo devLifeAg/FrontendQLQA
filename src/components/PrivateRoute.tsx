e771a0523a42d22525a50e1636fc600b02ef5434
Phong <phongchau346@gmail.com>
2025-04-14 08:49:37 +0700

    eidt ko tran vien, xoa

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -9,10 +9,10 @@ interface PrivateRouteProps {
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children, disallowedRoles }) => {
   const user = JSON.parse(localStorage.getItem("user") || "null");
 
-  // if (!user) {
-  //   // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
-  //   return <Navigate to="/" />;
-  // }
+  if (!user) {
+    // Nếu chưa đăng nhập, chuyển hướng đến trang đăng nhập
+    return <Navigate to="/" />;
+  }
   if (disallowedRoles.includes(user.u_role)) {
     // Nếu đã đăng nhập nhưng role bị chặn, chuyển hướng đến trang chủ
     return <Navigate to="/Home" />;
